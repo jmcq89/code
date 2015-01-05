10883056df6a9fce581d3ccf6ffe4874260913f6
jmcq89 <jmcq89@gmail.com>
2015-01-05 12:12:21 -0800

    new files

---
+++ KRML/KRML.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class KRML:
+    """ This class contains all of the necessary functions
+    to estimate the embedding f(X) and kernel regression
+    embedding function based on points Y given input
+    data X, intrinsic dimension d, and output dimension s.
+    """"
+    
+    def __init__(self):
+        
+    def kernel():
+    
+    def loss():
+    
+    def gradient():
+    
+    def 
---
+++ KRML/README.txt
@@ -0,0 +1,8 @@
+This will be where the code for the 
+Kernel Regression Manifold Learning
+technique will be implemented.
+
+Plan:
+
+Class - KRML
+    - Inside class, creates functions
